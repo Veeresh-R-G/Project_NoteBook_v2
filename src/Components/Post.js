3cbc30385e3b05385e140a0889c5bf513077be3b
Veeresh R G <veeresh.ravi@gmail.com>
2023-06-02 00:21:52 +0530

    [EDIT] User Page :rocket:

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -5,18 +5,18 @@ const Post = ({ id,title, hLink, gLink, desc }) => {
 
     return (
 
-        <div className="bg-white w-full px-13 posts mx-2 rounded-lg my-2 p-5" key={id + 1}>
+        <div className="bg-white w-full px-13 posts rounded-lg my-4 px-3" key={id + 1}>
 
-            <h1 className="text-4xl font-semibold">{title}</h1>
-            <p className="text-lg mt-2 text-gray-500">{desc}</p>
+            <h1 className="text-xl md:text-4xl font-semibold">{title}</h1>
+            <p className="text-base md:text-lg mt-2 text-gray-500">{desc}</p>
 
             <div className="mt-5">
                 <div className="flex">
-                    <div className="mr-10">
+                    <div className="pr-10">
 
                         <div className="flex">
 
-                            <p className="text-lg mr-2">GitHub </p>
+                            <p className="text-base md:text-lg mr-2">GitHub </p>
                             <a href={gLink}>
                                 <span className="my-auto"><VscGithubInverted className=" h-5 w-5" /></span>
                             </a>
@@ -26,7 +26,7 @@ const Post = ({ id,title, hLink, gLink, desc }) => {
                     </div>
                     <div className="mr-10">
                         <div className="flex">
-                            <p className="text-lg mr-2">Website </p>
+                            <p className="text-base md:text-lg pr-2">Website </p>
                             <a href={hLink}>
                                 <span className="my-auto"><AiOutlineLink className=" h-5 w-5" /></span>
                             </a>
--- src/Pages/User.js
+++ src/Pages/User.js
@@ -10,29 +10,49 @@ const User = () => {
 
     const navigate = useNavigate();
     const [Data, setData] = useState(null);
+    const [dummyData , setDummy] = useState(null);
     useEffect(() => {
-
-        
-        // console.log(localStorage.getItem("doc_id"))
         const q = query(colRef , where("uuid" , "==" , localStorage.getItem("uuid")));
         getDocs(q).then((querySnapshot) => {
             
             querySnapshot.forEach((doc) => {
                 localStorage.setItem("doc_id" , doc.id);
                 setData(doc.data().projects.reverse());
+                setDummy(doc.data().projects.reverse());
             });
         });
-        // const getRecords = async () => {
-        //     const data = await getDocs(colRef);
-        //     setData(data.docs.map((doc) => ({ ...doc.data() })))
-        // }
-
-        // getRecords();
     }
     , [])
 
+    const selectProjects = (e) => 
+    {
+        const projects = e.target.value;
+        
+        if(projects === 'All')
+        {  
+            setDummy(Data);
+            return;
+        }
+
+        if(projects === 'r_5')
+        {
+            setDummy(Data.slice(0,5));
+            return;
+        }
+
+        if(projects === 'r_10')
+        {
+            setDummy(Data.slice(0 , 10));
+            return;
+        }
+
+
+
+
+
+    }
+
     const handleLogout = () => {
-        //clear the entire localstorage
         toast.success('Logged Out Successfully !!!' , 
         {
             position: "top-right",
@@ -52,7 +72,7 @@ const User = () => {
     let photoUrl = localStorage.Photo;
     return (
         <div className="bg-[#52b69a] min-h-screen pt-5 px-1">
-            <div className="bg-white flex justify-around py-3 mx-40 mb-3 rounded-lg ">
+            <div className="bg-white flex justify-around py-3 mx-2 md:mx-40 mb-3 rounded-lg ">
                 <div>
                     <h1 className="text-xl font-semibold text-[#359a7d]  flex items-center ">Hello,
                         {localStorage.Name}
@@ -65,11 +85,10 @@ const User = () => {
                 <div className="border-4 border-[#52b69a] p-1 rounded-full">
                     <img src={photoUrl} alt="" className="rounded-full w-16 h-16" />
                 </div>
-
-
-
             </div>
-            <div className="hidden md:grid md:grid-cols-3 gap-3">
+
+
+            <div className="flex flex-col-reverse md:grid md:grid-cols-3 gap-3">
                 <div className="md:col-span-2 ">
                     <div className="flex items-center justify-center">
 
@@ -77,14 +96,24 @@ const User = () => {
 
                     </div>
                     
-                    {Data?.length === 0 ? 
+                    {dummyData?.length === 0 ? 
                     <>
                         <div className="bg-white rounded-xl text-center text-[#52b69a] font-semibold mt-4">
                             Add your First Project Now !!!
                         </div>
                     </> :
                     <>
-                    {Data?.map((item , ind) => {
+                    <div>
+                        <div>
+                            <select onChange={selectProjects} className="border-2 outline-none text-[#1a5b43] font-semibold border-[#52b69a] rounded-md px-2 py-1 mt-3 " name="filter" id="filter">
+                                <option value="All">All</option>
+                                <option value="r_5">Recent 5</option>
+                                <option value="r_10">Recent 10</option>
+                            </select>    
+                        </div>
+
+                    </div>
+                    {dummyData?.map((item , ind) => {
                             return (
                                 <Post key={ind} id={ind + 1} title={item?.title} gLink={item?.gLink} hLink={item?.hLink} desc={item?.desc} />
                             )
@@ -94,7 +123,7 @@ const User = () => {
                     }
             
                 </div>
-                <div className="md:col-span-1 mt-10 ">
+                <div className="md:col-span-1 mt-5 md:mt-14 ">
                     <Form DATA={Data} />
                 </div>
             </div>
